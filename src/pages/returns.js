2075f2ba081becb834ee24643b8adbed1676b3ce
Artie <turo310@gmail.com>
2019-12-12 18:01:48 -0800

    Apply style to return policy page

--- src/pages/returns.js
+++ src/pages/returns.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Returns = () => {
 
     return (
-        <div>
-            <div>
+        <div className="returns__container">
+            <h2>RETURN POLICY</h2>
+            <div className="returns__question-container">
                 <h3>What is your return policy?</h3>
                 <p>If you are unsatisfied with your purchase for any reason, you may return it within 30 days of 
                 delivery (delivery is defined as the date the shipment was marked delivered by the carrier’s tracking number). 
@@ -12,12 +13,12 @@ const Returns = () => {
                 offering FedEx return labels exclusively for all returns. Customers will be charged an $8.00 flat-rate return 
                 fee. We apologize for any inconvenience this may cause!</p>
             </div>
-            <div>
+            <div className="returns__question-container">
                 <h3>Who do I contact if I have questions about a return?</h3>
                 <p>For answers to any questions, please contact the NEXFIT Customer Support team at support@nexfitapparel.com 
                 between 9AM and 4PM, Monday through Friday. We’ll be happy to help!</p>
             </div>
-            <div>
+            <div className="returns__question-container">
                 <h3>Is there a restocking fee for returns?</h3>
                 <p>No, we do not charge a restocking fee for any returns received within 30 days.</p>
             </div>
