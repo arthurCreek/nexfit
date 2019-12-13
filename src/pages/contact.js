32e7b1c7ebcd8f36c279bb21bdcfe356b0df94d5
Artie <turo310@gmail.com>
2019-12-12 18:18:54 -0800

    Add layout to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,16 +3,16 @@ import React from 'react';
 const Contact = () => {
 
     return (
-        <div>
+        <div className="contact__container">
             <h1>CONTACT</h1>
             <p>Contact the team at NEXFIT to ask questions about your order, or to learn more about our outdoor 
             apparel that uses proprietary aerogel technology, NASA-inspired science that delivers results.</p>
             <p>Do you have questions or need help?</p>
-            <div>
+            <div className="contact__info">
                 <h3>Email us </h3>
                 <p>support@nexfitapparel.com.</p>
             </div>
-            <div>
+            <div className="contact__info">
                 <h3>Call us </h3>
                 <p>Monday - Friday</p>
                 <p>10am - 4pm ET</p>
