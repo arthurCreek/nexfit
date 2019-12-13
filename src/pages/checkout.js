cb48eef6b9675da6a4aa68f1b4d3f046697218e9
Artie <turo310@gmail.com>
2019-12-12 19:16:29 -0800

    Add layout to checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -16,83 +16,80 @@ const Checkout = () => {
         <div>
             <div className="checkout__container">
                 <div className="checkout__payment-container">
-                    <h1>NexFit</h1>
-                    <p>Cart > Information > Shippinh > Payment</p>
-                    <h2>Contact information</h2>
-                    <form action="#" name="contact_form">
-                        <label for="email">Email</label>
-                        <input name="email" type="email" required placeholder="you@domain.com"/>
-                        <br />
-                        <div className="form__side-by-side">
-                            <div>
-                                <label for="first_name">First Name</label>
-                                <input name="first_name" type="text" required placeholder="John"/>
+                    <div className="checkout__payment-container-center">
+                        <h1>NexFit</h1>
+                        <p>Cart > <span className="checkout_text-bold">Information</span> > Shipping > Payment</p>
+                        <h2>Contact information</h2>
+                        <form action="#" name="contact_form">
+                            <div className="contact__email-container">
+                                <label for="email"></label>
+                                <input name="email" type="email" required placeholder="Email" className="payment__input--large"/>
+
+                                <div className="contact__marketing-container">
+                                    <input type="checkbox" name="marketing"/>
+                                    <label for="marketing">Keep me up to date on news and exclusive offers</label>
+                                </div>
                             </div>
-                            <div>
-                                <label for="last_name">Last Name</label>
-                                <input name="last_name" type="text" required placeholder="Doe"/>
-                            </div>
-                        </div>
 
-                        <input type="checkbox" name="marketing"/>
-                        <label for="marketing">Keep me up to date on news and exclusive offers</label>
-
-                        <br />
-
-                        <h2>Shipping</h2>
-                        <div className="form__side-by-side">
-                            <div>
-                                <label for="first_name_shipping"></label>
-                                <input name="first_name_shipping" type="text" required placeholder="First Name"/>
-                            </div>
-                            <div>
-                                <label for="last_name_shipping"></label>
-                                <input name="last_name_shipping" type="text" required placeholder="Last Name"/>
-                            </div>
-                        </div>
-
-                        <div>
-                            <label for="company"></label>
-                            <input name="company" type="text" placeholder="Company (optional)"/>
-                        </div>
-
-                        <div>
-                            <label for="address"></label>
-                            <input name="address" type="text" required placeholder="Address"/>
-                        </div>
-
-                        <div>
-                            <label for="apartment"></label>
-                            <input name="apartment" type="text" placeholder="Apartment, suite, etc. (optional)"/>
-                        </div>
-
-                        <div className="form__side-by-side">
-                            <div>
-                                <label for="city"></label>
-                                <input name="city" type="text" required placeholder="City"/>
+                            <h2 className="checkout__h2">Shipping</h2>
+                            <div className="form__side-by-side">
+                                <div className="form__side-by-side--inputs">
+                                    <label for="first_name_shipping"></label>
+                                    <input name="first_name_shipping" type="text" required placeholder="First Name" className="payment__input--small"/>
+                                </div>
+                                <div>
+                                    <label for="last_name_shipping"></label>
+                                    <input name="last_name_shipping" type="text" required placeholder="Last Name" className="payment__input--small"/>
+                                </div>
                             </div>
 
                             <div>
-                                <label for="state"></label>
-                                <input name="state" type="text" required placeholder="State (CA)"/>
+                                <label for="company"></label>
+                                <input name="company" type="text" placeholder="Company (optional)" className="payment__input--large"/>
                             </div>
-                        </div>
 
-                        <div>
-                            <label for="zip"></label>
-                            <input name="zip" type="text" required placeholder="ZIP Code"/>
-                        </div>
+                            <div>
+                                <label for="address"></label>
+                                <input name="address" type="text" required placeholder="Address" className="payment__input--large"/>
+                            </div>
 
-                        <div>
-                            <label for="phone"></label>
-                            <input name="phone" type="text" required placeholder="Phone"/>
-                        </div>
+                            <div>
+                                <label for="apartment"></label>
+                                <input name="apartment" type="text" placeholder="Apartment, suite, etc. (optional)" className="payment__input--large"/>
+                            </div>
 
-                        <div class="center">
-                            <Link href="#"><a>Return to cart</a></Link>
-                            <Link href="#"><a>Continue Checkout</a></Link>
-                        </div>
-                    </form>	
+                            <div>
+                                <div>
+                                    <label for="city"></label>
+                                    <input name="city" type="text" required placeholder="City" className="payment__input--large"/>
+                                </div>
+
+
+                            </div>
+
+                            <div className="form__side-by-side">
+                                <div className="form__side-by-side--inputs">
+                                    <label for="state"></label>
+                                    <input name="state" type="text" required placeholder="State (CA)" className="payment__input--small"/>
+                                </div>
+
+                                <div>
+                                    <label for="zip"></label>
+                                    <input name="zip" type="text" required placeholder="ZIP Code" className="payment__input--small"/>
+                                </div>
+                            </div>
+
+                            <div>
+                                <label for="phone"></label>
+                                <input name="phone" type="text" required placeholder="Phone" className="payment__input--large"/>
+                            </div>
+
+                            <div className="checkout__footer-checkout">
+                                <Link href="#"><a className="checkout-return">Return to cart</a></Link>
+                                <Link href="#"><a className="btn-checkout">Continue Checkout</a></Link>
+                            </div>
+                        </form>	
+                    </div>
                 </div>
 
                 <div className="checkout__main-item-container">
@@ -113,7 +110,6 @@ const Checkout = () => {
                     </div>
                     <div className="checkout__item-footer">
                         <p>Total: ${total}</p>
-                        <Link href="/checkout"><a className="btn-checkout">Checkout</a></Link>
                     </div>
                 </div>
             </div>
